a39401971784b3778e55005419d0cf701b4738e6
Soubhik Ghosh <99ghoshsoubhik@gmail.com>
2024-11-12 14:38:17 +0530

    background animation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'code';
+  title = 'V.S. Tech Solutions';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseModule } from './modules/base/base.module';
 import { SharedModule } from './modules/shared/shared.module';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { SharedModule } from './modules/shared/shared.module';
     AppRoutingModule,
     BaseModule,
     SharedModule,
+    HomeModule,
     TranslateModule.forRoot({
       defaultLanguage: 'en'
   })
