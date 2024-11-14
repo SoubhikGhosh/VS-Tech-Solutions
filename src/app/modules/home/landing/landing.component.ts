9f0f3c61e6534562b313b7eda586fec286f01bdd
Soubhik Ghosh <99ghoshsoubhik@gmail.com>
2024-11-15 01:34:03 +0530

    added fade out

--- src/app/modules/home/landing/landing.component.ts
+++ src/app/modules/home/landing/landing.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrl: "./landing.component.scss",
 })
 export class LandingComponent {
+  constructor(private router: Router) {}
+
+  navigateWithFade(container: HTMLDivElement) {
+    container.classList.add("fade-out");
+    setTimeout(() => {
+      this.router.navigate(["/details"]);
+    }, 800);
+  }
 }
