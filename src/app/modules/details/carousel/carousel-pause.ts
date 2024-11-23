2ddd53e209a5e700f8ddd63245618740b22a6449
Soubhik Ghosh <99ghoshsoubhik@gmail.com>
2024-11-23 23:41:16 +0530

    final v1

--- src/app/modules/details/carousel/carousel-pause.ts
+++ src/app/modules/details/carousel/carousel-pause.ts
@@ -13,21 +13,27 @@ import { CommonModule } from "@angular/common";
   standalone: true,
   imports: [NgbCarouselModule, FormsModule, CommonModule],
   templateUrl: "./carousel-pause.html",
-  styleUrl: './carousel-pause.scss'
-
+  styleUrl: "./carousel-pause.scss",
 })
 export class NgbdCarouselPause {
+  @ViewChild("carousel", { static: true }) carousel!: NgbCarousel;
+
   carouselItems = [
     {
       image: "/assets/images/FE_Tech.png",
       heading: "Frontend Development",
-      content: "This is the content for slide 1",
+      content: "Unleash the power of modern frontend development with VS Tech Solutions. Mastering ReactJS, Angular, TypeScript, and more, we deliver sleek, responsive, and visually captivating websites tailored to elevate your brand. Let cutting-edge technology and limitless creativity bring your digital vision to life.",
     },
     {
-        image: "/assets/images/FE_Tech.png",
-        heading: "Backend Development",
-        content: "This is the content for slide 2",
-      }
+      image: "/assets/images/BE_Tech.png",
+      heading: "Backend Development",
+      content: "Transform your digital solutions with VS Tech Solutions' backend expertise. Leveraging Java, Python, Node.js, MySQL, and MongoDB, we craft secure, scalable architectures. With cutting-edge APIs and database solutions, we ensure seamless, high-performance experiences skillfully tailored to your exact needs.",
+    },
+    {
+      image: "/assets/images/UX_Tech.png",
+      heading: "UI/UX Development",
+      content: "Revolutionize user experiences with VS Tech Solutions' UI/UX design expertise. Leveraging Figma, Adobe XD, and cutting-edge methodologies like user-centric design, micro-interactions, and responsive prototyping, we craft intuitive, visually compelling interfaces that captivate and engage seamlessly.",
+    },
   ];
 
   paused = false;
@@ -35,8 +41,6 @@ export class NgbdCarouselPause {
   pauseOnIndicator = false;
   pauseOnHover = true;
   pauseOnFocus = true;
-
-  @ViewChild("carousel", { static: true }) carousel!: NgbCarousel;
 
   togglePaused() {
     if (this.paused) {
